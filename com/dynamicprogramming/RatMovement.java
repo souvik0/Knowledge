6e352c833ab49aa487d5d55ff57100eb98c37d2c
Souvik Sen <sensouvik0112@gmail.com>
2022-06-11 23:44:41 +0530

    Updated variable name to more meaningful one

--- com/dynamicprogramming/RatMovement.java
+++ com/dynamicprogramming/RatMovement.java
@@ -2,10 +2,6 @@ package com.dynamicprogramming;
 // Here rat can move in left, right, bottom & up direction by 1 step each
 public class RatMovement {
 
-    // Define rat moves
-    public static int[] Path_Row = {0, 0, 1, -1};
-    public static int[] Path_Col = {1, -1, 0, 0};
-
     public static void main(String[] args) {
         int maze[][] = {{1, 1, 0, 1 },
                         {0, 1, 1, 1 },
@@ -24,27 +20,30 @@ public class RatMovement {
     }
 
     public static void pathInMaze(int[][] maze, int[][] visited, int startRow, int startCol, 
-                                   int destRow, int destCol, int move) {
+                                   int destRow, int destCol, int movePointer) {
         if ((startRow == 0) && (startCol ==0)) {
-            visited[startRow][startCol] = move;
+            visited[startRow][startCol] = movePointer;
         }
 
         // Recursion break condition
         if ((startRow == destRow) && (startCol == destCol)) {
             printSolution(visited);
         } else {
+            int[] Path_Row = {0, 0, 1, -1};
+            int[] Path_Col = {1, -1, 0, 0};
+
             for (int index = 0; index < Path_Row.length; index++) {
                 int rowNext = startRow + Path_Row[index];
                 int colNext = startCol + Path_Col[index];
                 // Rat can move only once in a cell while visiting
                 if (canRatMove(maze, visited, rowNext, colNext)) {
-                    move++;
-                    visited[rowNext][colNext] = move;
-                    pathInMaze(maze, visited, rowNext, colNext, destRow, destCol, move);
+                	movePointer++;
+                    visited[rowNext][colNext] = movePointer;
+                    pathInMaze(maze, visited, rowNext, colNext, destRow, destCol, movePointer);
                     // This is where we are doing backtracking.
                     // should go back to initial value of how visited array was created
                     // This needs to be done to get multiple paths
-                    move--;
+                    movePointer--;
                     visited[rowNext][colNext] = 0;
                 }
             }
@@ -52,7 +51,7 @@ public class RatMovement {
     }
 
     public static boolean canRatMove(int[][] maze, int[][] visited, int rowNext, int colNext) {
-        // If any cell in maze marked as o, then that is not movable cell.
+        // If any cell in maze marked as 0, then that is not movable cell.
         // If already visited a cell in visited matrix, then it can't be visited twice
         if ((rowNext >= 0) && (rowNext < maze.length) && (colNext >= 0) && (colNext < maze[0].length) && 
             maze[rowNext][colNext] == 1 && visited[rowNext][colNext] == 0) {
